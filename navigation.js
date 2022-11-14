a80b54371db1533b23a886ffc476c9ccaf00c746
Roy Jumah <royjumah17@gmail.com>
2022-11-14 11:54:32 +0300

    Add click fuctionality to navigation link

--- navigation.js
+++ navigation.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// Making mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
