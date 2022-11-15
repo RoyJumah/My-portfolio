c153c4ce98ca33b93e26d39967e06f7149eaedab
Roy Jumah <royjumah17@gmail.com>
2022-11-15 22:36:58 +0300

    third commit

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
 "use strict";
 
- const projects = [
+const projects = [
   {
     name: "Topic",
     feature: "CANOPY",
@@ -63,40 +63,40 @@
 
 const startingPoint = document.querySelector("main");
 
- function createModal() {
+function createModal() {
   startingPoint.insertAdjacentHTML(
     "afterend",
     `<div class='overlay'>
     <section class='modal'>
-    <div class='modal__header flex'>
-    <h2 class='project-name fw-700 fz-32 lh-44 clr-n-800'></h2>
+    <div class='modal-header flex'>
+    <h2 class='project-name title fw-700 fz-32 lh-44 clr-n-800'></h2>
     <button class="closeModal">&times;</button>
     </div>
     <div class='modal__feature flex'>
-    <p class="project-feature fw-600 fz-13 lh-16 clr-n-600"></p>
-    <ul class=" flex">
+    <ul class=" portfolio-list flex">
+    <p class="project-feature fw-600 fz-13 lh-16 clr-n-600 list-unstyled"></p>
     <li class=" project-background fw-600 fz-13 lh-16 clr-n-500"></li>
     <li class="project-year fw-600 fz-13 lh-16 clr-n-500"> </li>
     </ul></div>
     <div class='image-project'><img class='modal__image project-image' src='' alt='project image'></img></div>
     <ul class='modal__dsc ul flex'>
-    <li class='modal__dsc__first'><p class='project-description fw-400 fz-15 lh-24 clr-n-600'></p></li>
-    <li class="modal__dsc__second flex">
-    <div class='flex'>
-    <p class="project-tool1 fw-500 fz-12 lh-16 clr-p-400 bg-p-50"></p>
-    <p class="project-tool2 fw-500 fz-12 lh-16 clr-p-400 bg-p-50"></p>
-    <p class="project-tool3 fw-500 fz-12 lh-16 clr-p-400 bg-p-50"></p>
-    </div>
-    <ul class='modal__btn ul flex'>
-    <li>
+    <p class='modal__dsc__first'><p class='project-description portfolio-info fw-400 fz-15 lh-24 clr-n-600'></p></p>
+    
+    <ul class='languages-list'>
+    <li class="project-tool1 fw-500 fz-12 lh-16 clr-p-400 bg-p-50"></li>
+    <li class="project-tool2 fw-500 fz-12 lh-16 clr-p-400 bg-p-50"></li>
+    <li class="project-tool3 fw-500 fz-12 lh-16 clr-p-400 bg-p-50"></li>
+    </ul>
+
+    
+    <div class=" btn-wrapper">
     <a href='' class="btn link project-linkLive fw-500 fz-17 lh-24 clr-p-500 bg-white">See Live</a>
-    </li>
-    <li>
+    
+    
     <a href='' class="btn link project-linkSource fw-500 fz-17 lh-24 clr-p-500 bg-white">See Source</a>
-    </li>
-    </ul>
-    </li>
-    </ul>
+    
+    
+
     </section>
     </div>`
   );
