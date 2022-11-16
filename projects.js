d9bc4440e87360b8a8444e783b803b27ded8e349
Roy Jumah <royjumah17@gmail.com>
2022-11-16 11:40:40 +0300

    Fix ES linter errors

--- projects.js
+++ projects.js
@@ -1,61 +1,61 @@
 const projects = [
   {
-    name: "Topic",
-    feature: "CANOPY",
-    background: "Back End Dev",
-    year: "2015",
-    image: "./images/project6.png",
+    name: 'Topic',
+    feature: 'CANOPY',
+    background: 'Back End Dev',
+    year: '2015',
+    image: './images/project6.png',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    tool1: "html",
-    tool2: "css",
-    tool3: "javascript",
-    linkLive: "https://royjumah.github.io/My-portfolio",
-    linkSource: "https://github.com/RoyJumah/My-portfolio",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tool1: 'html',
+    tool2: 'css',
+    tool3: 'javascript',
+    linkLive: 'https://royjumah.github.io/My-portfolio',
+    linkSource: 'https://github.com/RoyJumah/My-portfolio',
   },
 
   {
-    name: "Multi-Post Stories",
-    feature: "FACEBOOK",
-    background: "Full Stack Dev",
-    year: "2015",
-    image: "./images/project2.png",
+    name: 'Multi-Post Stories',
+    feature: 'FACEBOOK',
+    background: 'Full Stack Dev',
+    year: '2015',
+    image: './images/project2.png',
     description:
-      "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-    tool1: "html",
-    tool2: "css",
-    tool3: "javascript",
-    linkLive: "https://royjumah.github.io/My-portfolio",
-    linkSource: "https://github.com/RoyJumah/My-portfolio",
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    tool1: 'html',
+    tool2: 'css',
+    tool3: 'javascript',
+    linkLive: 'https://royjumah.github.io/My-portfolio',
+    linkSource: 'https://github.com/RoyJumah/My-portfolio',
   },
 
   {
-    name: "Facebook 360",
-    feature: "FACEBOOK",
-    background: "Full Stack Dev",
-    year: "2015",
-    image: "./images/project3.png",
+    name: 'Facebook 360',
+    feature: 'FACEBOOK',
+    background: 'Full Stack Dev',
+    year: '2015',
+    image: './images/project3.png',
     description:
       "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    tool1: "html",
-    tool2: "css",
-    tool3: "javascript",
-    linkLive: "https://royjumah.github.io/My-portfolio",
-    linkSource: "https://github.com/RoyJumah/My-portfolio",
+    tool1: 'html',
+    tool2: 'css',
+    tool3: 'javascript',
+    linkLive: 'https://royjumah.github.io/My-portfolio',
+    linkSource: 'https://github.com/RoyJumah/My-portfolio',
   },
   {
-    name: "Uber Navigation",
-    feature: "Uber",
-    background: "Lead Developer",
-    year: "2018",
-    image: "./images/project4.png",
+    name: 'Uber Navigation',
+    feature: 'Uber',
+    background: 'Lead Developer',
+    year: '2018',
+    image: './images/project4.png',
     description:
-      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
-    tool1: "html",
-    tool2: "css",
-    tool3: "javascript",
-    linkLive: "https://royjumah.github.io/My-portfolio/",
-    linkSource: "https://github.com/RoyJumah/My-portfolio",
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    tool1: 'html',
+    tool2: 'css',
+    tool3: 'javascript',
+    linkLive: 'https://royjumah.github.io/My-portfolio/',
+    linkSource: 'https://github.com/RoyJumah/My-portfolio',
   },
 ];
 const startingPoint = document.querySelector('main');
@@ -120,8 +120,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.scroll.overflow = '';
   };
 
-
-buttonsProject.forEach((button) => {
+  buttonsProject.forEach((button) => {
     button.addEventListener('click', () => {
       if (button.id === '0') {
         projectName.textContent = `${projects[button.id].name}`;
