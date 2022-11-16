16d9602209feaf7ff8e6e8aa0b9837c25b456f25
habasm <habasm365@gmail.com>
2022-11-16 11:14:43 +0300

    Add modal to the portfolio

--- projects.js
+++ projects.js
@@ -58,3 +58,38 @@ const projects = [
     linkSource: "https://github.com/RoyJumah/My-portfolio",
   },
 ];
+const startingPoint = document.querySelector('main');
+
+function createModal() {
+  startingPoint.insertAdjacentHTML(
+    'afterend',
+    `<div class='overlay'>
+    <section class='modal'>
+    <div class='modal-header'>
+    <h2 class='project-name title'></h2>
+    <button class="closeModal">&times;</button>
+    </div>
+    <ul class='modal__feature portfolio-list'>
+    <li class="project-feature name"></li>
+    <li class=" project-background"></li>
+    <li class="project-year"> </li>
+    </ul>
+    <div class='image-project'><img class='modal__image project-image' src='' alt='project image'></img></div>
+    <div class='modal__dsc '>
+    <p class='modal__dsc__first'><p class='project-description portfolio-info fw-400 fz-15 lh-24 clr-n-600'></p></p>
+  <div>
+    <ul class='languages-list margin-top'>
+    <li class="project-tool1"></li>
+    <li class="project-tool2"></li>
+    <li class="project-tool3"></li>
+    </ul>
+    <div class=" btn-wrapper margin-top">
+    <a href='' class="btn link project-linkLive">See Live</a>
+    <a href='' class="btn link project-linkSource">See Source</a>
+    </div>
+    </div>
+    </div>
+    </section>
+    </div>`,
+  );
+}
