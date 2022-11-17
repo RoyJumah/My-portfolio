77b2a9b79f574bc81a81df17af67dd1642c926ba
Roy Jumah <royjumah17@gmail.com>
2022-11-17 19:18:57 +0300

    Change to descriptive variable name in form validation

--- validation.js
+++ validation.js
@@ -2,8 +2,8 @@ function containsUppercase(str) {
   return /[A-Z]/.test(str);
 }
 function validateForm() {
-  let x = document.forms["myForm"]["contact_email"].value;
-  if (containsUppercase(x)) {
+  const emailEntered = document.forms["myForm"]["contact_email"].value;
+  if (containsUppercase(emailEntered)) {
     document.getElementById("err_info").innerHTML =
       "Email should not contain a capital letter!";
     return false;
