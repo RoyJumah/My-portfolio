5f89398d4556dbd2ca928171b6e3dbff6950dac1
Roy Jumah <royjumah17@gmail.com>
2022-11-18 08:37:57 +0300

    Fix ES linting errors

--- validation.js
+++ validation.js
@@ -1,11 +1,14 @@
+const form = document.getElementById('form');
+
 function containsUppercase(str) {
   return /[A-Z]/.test(str);
 }
-function validateForm() {
-  const emailEntered = document.forms["myForm"]["contact_email"].value;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const emailEntered = document.forms.myForm.contact_email.value;
   if (containsUppercase(emailEntered)) {
-    document.getElementById("err_info").innerHTML =
-      "Email should not contain a capital letter!";
-    return false;
+    document.getElementById('err_info').innerHTML = 'Email should not contain a capital letter!';
   }
-}
+  return false;
+});
