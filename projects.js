1a0c04eb8bbf6836d21493d20e12302f7bb89a27
Roy Jumah <royjumah17@gmail.com>
2022-12-14 18:57:43 +0300

    Add new projects to portfolio

--- projects.js
+++ projects.js
@@ -1,69 +1,69 @@
 const projects = [
   {
-    name: 'Topic',
-    feature: 'CANOPY',
-    background: 'Back End Dev',
-    year: '2015',
-    image: './images/project6.png',
+    name: "Coachella Concert Showcase",
+    feature: "Coachella Concert",
+    background: "Front End Dev",
+    year: "2022",
+    image: "./images/coachella-snapshot.png",
     description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    tool1: 'html',
-    tool2: 'css',
-    tool3: 'javascript',
-    linkLive: 'https://royjumah.github.io/My-portfolio',
-    linkSource: 'https://github.com/RoyJumah/My-portfolio',
+      "The Coachella Concert Showcase is a simple project aimed to represent an annual concert in America. It has only two pages, the home page and the about page.",
+    tool1: "html",
+    tool2: "css",
+    tool3: "javascript",
+    linkLive: "https://royjumah.github.io/Coachella-Capstone-project/",
+    linkSource: "https://github.com/RoyJumah/Coachella-Capstone-project",
   },
 
   {
-    name: 'Multi-Post Stories',
-    feature: 'FACEBOOK',
-    background: 'Full Stack Dev',
-    year: '2015',
-    image: './images/project2.png',
+    name: "Awesome Books List",
+    feature: "Awesome Books",
+    background: "Front End Dev",
+    year: "2022",
+    image: "./images/awesome-books.png",
     description:
-      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    tool1: 'html',
-    tool2: 'css',
-    tool3: 'javascript',
-    linkLive: 'https://royjumah.github.io/My-portfolio',
-    linkSource: 'https://github.com/RoyJumah/My-portfolio',
+      "This is a project created using HTML, CSS and Javascript that enables a user to add and remove a book from a list",
+    tool1: "html",
+    tool2: "css",
+    tool3: "javascript",
+    linkLive: "https://royjumah.github.io/Awesome-Books-ES6/#",
+    linkSource: "https://github.com/RoyJumah/Awesome-Books-ES6",
   },
 
   {
-    name: 'Facebook 360',
-    feature: 'FACEBOOK',
-    background: 'Full Stack Dev',
-    year: '2015',
-    image: './images/project3.png',
+    name: "Facebook 360",
+    feature: "FACEBOOK",
+    background: "Full Stack Dev",
+    year: "2015",
+    image: "./images/project3.png",
     description:
       "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    tool1: 'html',
-    tool2: 'css',
-    tool3: 'javascript',
-    linkLive: 'https://royjumah.github.io/My-portfolio',
-    linkSource: 'https://github.com/RoyJumah/My-portfolio',
+    tool1: "html",
+    tool2: "css",
+    tool3: "javascript",
+    linkLive: "https://royjumah.github.io/My-portfolio",
+    linkSource: "https://github.com/RoyJumah/My-portfolio",
   },
   {
-    name: 'Uber Navigation',
-    feature: 'Uber',
-    background: 'Lead Developer',
-    year: '2018',
-    image: './images/project4.png',
+    name: "Uber Navigation",
+    feature: "Uber",
+    background: "Lead Developer",
+    year: "2018",
+    image: "./images/project4.png",
     description:
-      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    tool1: 'html',
-    tool2: 'css',
-    tool3: 'javascript',
-    linkLive: 'https://royjumah.github.io/My-portfolio/',
-    linkSource: 'https://github.com/RoyJumah/My-portfolio',
+      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    tool1: "html",
+    tool2: "css",
+    tool3: "javascript",
+    linkLive: "https://royjumah.github.io/My-portfolio/",
+    linkSource: "https://github.com/RoyJumah/My-portfolio",
   },
 ];
 
-const startingPoint = document.querySelector('main');
+const startingPoint = document.querySelector("main");
 
 function createModal() {
   startingPoint.insertAdjacentHTML(
-    'afterend',
+    "afterend",
     `<div class='overlay'>
     <section class='modal'>
     <div class='modal-header'>
@@ -91,108 +91,108 @@ function createModal() {
     </div>
     </div>
     </section>
-    </div>`,
+    </div>`
   );
 }
 
 // Event that detects when the page loads
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   createModal();
 
-  const buttonsProject = document.querySelectorAll('button');
-  const closeIcon = document.querySelector('.closeModal');
-  const modal = document.querySelector('.modal');
-  const backgroundModal = document.querySelector('.overlay');
-  const projectName = document.querySelector('.project-name');
-  const projectFeature = document.querySelector('.project-feature');
-  const projectBack = document.querySelector('.project-background');
-  const projectYear = document.querySelector('.project-year');
-  const projectImage = document.querySelector('.project-image');
-  const projectDescription = document.querySelector('.project-description');
-  const projectTool1 = document.querySelector('.project-tool1');
-  const projectTool2 = document.querySelector('.project-tool2');
-  const projectTool3 = document.querySelector('.project-tool3');
-  const projectLinkLive = document.querySelector('.project-linkLive');
-  const projectLinkSource = document.querySelector('.project-linkSource');
+  const buttonsProject = document.querySelectorAll("button");
+  const closeIcon = document.querySelector(".closeModal");
+  const modal = document.querySelector(".modal");
+  const backgroundModal = document.querySelector(".overlay");
+  const projectName = document.querySelector(".project-name");
+  const projectFeature = document.querySelector(".project-feature");
+  const projectBack = document.querySelector(".project-background");
+  const projectYear = document.querySelector(".project-year");
+  const projectImage = document.querySelector(".project-image");
+  const projectDescription = document.querySelector(".project-description");
+  const projectTool1 = document.querySelector(".project-tool1");
+  const projectTool2 = document.querySelector(".project-tool2");
+  const projectTool3 = document.querySelector(".project-tool3");
+  const projectLinkLive = document.querySelector(".project-linkLive");
+  const projectLinkSource = document.querySelector(".project-linkSource");
 
   const closeModal = () => {
-    modal.classList.remove('active');
-    backgroundModal.classList.remove('active');
+    modal.classList.remove("active");
+    backgroundModal.classList.remove("active");
   };
 
   buttonsProject.forEach((button) => {
-    button.addEventListener('click', () => {
-      if (button.id === '0') {
+    button.addEventListener("click", () => {
+      if (button.id === "0") {
         projectName.textContent = `${projects[button.id].name}`;
         projectFeature.textContent = `${projects[button.id].feature}`;
         projectBack.textContent = `${projects[button.id].background}`;
         projectYear.textContent = `${projects[button.id].year}`;
-        projectImage.setAttribute('src', `${projects[button.id].image}`);
+        projectImage.setAttribute("src", `${projects[button.id].image}`);
         projectDescription.textContent = `${projects[button.id].description}`;
         projectTool1.textContent = `${projects[button.id].tool1}`;
         projectTool2.textContent = `${projects[button.id].tool2}`;
         projectTool3.textContent = `${projects[button.id].tool3}`;
-        projectLinkLive.setAttribute('href', `${projects[button.id].linkLive}`);
+        projectLinkLive.setAttribute("href", `${projects[button.id].linkLive}`);
         projectLinkSource.setAttribute(
-          'href',
-          `${projects[button.id].linkSource}`,
+          "href",
+          `${projects[button.id].linkSource}`
         );
-        modal.classList.add('active');
-        backgroundModal.classList.add('active');
-      } else if (button.id === '1') {
+        modal.classList.add("active");
+        backgroundModal.classList.add("active");
+      } else if (button.id === "1") {
         projectName.textContent = `${projects[button.id].name}`;
         projectFeature.textContent = `${projects[button.id].feature}`;
         projectBack.textContent = `${projects[button.id].background}`;
         projectYear.textContent = `${projects[button.id].year}`;
-        projectImage.setAttribute('src', `${projects[button.id].image}`);
+        projectImage.setAttribute("src", `${projects[button.id].image}`);
         projectDescription.textContent = `${projects[button.id].description}`;
         projectTool1.textContent = `${projects[button.id].tool1}`;
         projectTool2.textContent = `${projects[button.id].tool2}`;
         projectTool3.textContent = `${projects[button.id].tool3}`;
-        projectLinkLive.setAttribute('href', `${projects[button.id].linkLive}`);
+        projectLinkLive.setAttribute("href", `${projects[button.id].linkLive}`);
         projectLinkSource.setAttribute(
-          'href',
-          `${projects[button.id].linkSource}`,
+          "href",
+          `${projects[button.id].linkSource}`
         );
-        modal.classList.add('active');
-        backgroundModal.classList.add('active');
-      } else if (button.id === '2') {
+        modal.classList.add("active");
+        backgroundModal.classList.add("active");
+      } else if (button.id === "2") {
         projectName.textContent = `${projects[button.id].name}`;
         projectFeature.textContent = `${projects[button.id].feature}`;
         projectBack.textContent = `${projects[button.id].background}`;
         projectYear.textContent = `${projects[button.id].year}`;
-        projectImage.setAttribute('src', `${projects[button.id].image}`);
+        projectImage.setAttribute("src", `${projects[button.id].image}`);
         projectDescription.textContent = `${projects[button.id].description}`;
         projectTool1.textContent = `${projects[button.id].tool1}`;
         projectTool2.textContent = `${projects[button.id].tool2}`;
         projectTool3.textContent = `${projects[button.id].tool3}`;
-        projectLinkLive.setAttribute('href', `${projects[button.id].linkLive}`);
+        projectLinkLive.setAttribute("href", `${projects[button.id].linkLive}`);
         projectLinkSource.setAttribute(
-          'href',
-          `${projects[button.id].linkSource}`,
+          "href",
+          `${projects[button.id].linkSource}`
         );
-        modal.classList.add('active');
-        backgroundModal.classList.add('active');
-      } else if (button.id === '3') {
+        modal.classList.add("active");
+        backgroundModal.classList.add("active");
+      } else if (button.id === "3") {
         projectName.textContent = `${projects[button.id].name}`;
         projectFeature.textContent = `${projects[button.id].feature}`;
         projectBack.textContent = `${projects[button.id].background}`;
         projectYear.textContent = `${projects[button.id].year}`;
-        projectImage.setAttribute('src', `${projects[button.id].image}`);
+        projectImage.setAttribute("src", `${projects[button.id].image}`);
         projectDescription.textContent = `${projects[button.id].description}`;
         projectTool1.textContent = `${projects[button.id].tool1}`;
         projectTool2.textContent = `${projects[button.id].tool2}`;
         projectTool3.textContent = `${projects[button.id].tool3}`;
-        projectLinkLive.setAttribute('href', `${projects[button.id].linkLive}`);
+        projectLinkLive.setAttribute("href", `${projects[button.id].linkLive}`);
         projectLinkSource.setAttribute(
-          'href',
-          `${projects[button.id].linkSource}`,
+          "href",
+          `${projects[button.id].linkSource}`
         );
-        modal.classList.add('active');
-        backgroundModal.classList.add('active');
+        modal.classList.add("active");
+        backgroundModal.classList.add("active");
       }
     });
   });
 
-  closeIcon.addEventListener('click', closeModal);
+  closeIcon.addEventListener("click", closeModal);
 });
